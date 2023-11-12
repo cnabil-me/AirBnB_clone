893b23d4a58afa522d1fdc6702dbfc4cf2948455
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-11-12 13:23:40 -0800

    Iyad code

--- models/place.py
+++ models/place.py
@@ -1,41 +1,34 @@
 #!/usr/bin/python3
-"""
-Module: place.py
-
-Public class:
-    Place - a class that inherits from BaseModel and defines attributes for
-    Place objects.
-"""
+'''Write a class User that inherits from BaseModel'''
 from models.base_model import BaseModel
 
 
 class Place(BaseModel):
-    """
-    Place class that inherits from BaseModel
-    representing a place/house in the application.
+    '''Place Class
 
     Attributes:
-        name
-        user_id
-        city_id
-        description
-        number_bathrooms
-        price_by_night
-        number_rooms
-        longitude
-        latitude
-        max_guest
-        amenity_ids
-    """
-
-    name = ""
-    user_id = ""
-    city_id = ""
-    description = ""
-    number_bathrooms = 0
-    price_by_night = 0
+        Public class attributes:
+        city_id: string - empty string: it will be the City.id
+        user_id: string - empty string: it will be the User.id
+        name: string - empty string
+        description: string - empty string
+        number_rooms: integer - 0
+        number_bathrooms: integer - 0
+        max_guest: integer - 0
+        price_by_night: integer - 0
+        latitude: float - 0.0
+        longitude: float - 0.0
+        amenity_ids: list of string - empty list:
+            it will be the list of Amenity.id later
+    '''
+    city_id = ''
+    user_id = ''
+    name = ''
+    description = ''
     number_rooms = 0
-    longitude = 0.0
-    latitude = 0.0
+    number_bathrooms = 0
     max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
--- models/state.py
+++ models/state.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-"""
-Module: state.py
-Public class: State - a class that inherits from BaseModel and defines
-attributes for a state in the application
-"""
+'''Write a class User that inherits from BaseModel'''
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """
-    State class that inherits from BaseModel representing a state in the
-    application.
-    Attributes: name
-    """
-    name = ""
+    '''Place State
+
+    Attributes:
+        Public class attributes:
+        name: string - empty string
+    '''
+    name = ''
